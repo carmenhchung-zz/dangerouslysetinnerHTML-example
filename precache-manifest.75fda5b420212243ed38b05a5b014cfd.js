1bc58c5d71d170c35774d1e591d7d0b7dd39609e
Carmen Chung <30306087+carmenhchung@users.noreply.github.com>
2020-01-13 12:53:54 +1100

    Build v1

---
+++ precache-manifest.75fda5b420212243ed38b05a5b014cfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ecb09af5d1f9016244fc2d235aaa9a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1081fe99a7318c74c7aa",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0f507b70f471a69793c7",
+    "url": "/static/js/2.610ae661.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.610ae661.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1081fe99a7318c74c7aa",
+    "url": "/static/js/main.9174867c.chunk.js"
+  },
+  {
+    "revision": "08d3b813a31db6f78b21",
+    "url": "/static/js/runtime-main.6598b11c.js"
+  }
+]);
